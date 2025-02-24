07bfeda752087a217c8413614fbc3589b76a3930
diegoturchi95 <diego.turchi@lansweeper.com>
2025-02-25 00:10:30 +0100

    initial commit

---
+++ app/app/src/main/java/com/tdiego/composechallenges/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.tdiego.composechallenges
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import com.tdiego.composechallenges.ui.theme.ComposechallengesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposechallengesTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Text(
+                        text = "Empty app template",
+                        modifier = Modifier.padding(innerPadding),
+                    )
+                }
+            }
+        }
+    }
+}
