322f38775a9b530ee2773831fbe79842c33d7c26
diegoturchi95 <diego.turchi@lansweeper.com>
2025-02-26 18:14:49 +0100

    Created a ShimmeringCard component

--- app/app/src/main/java/com/tdiego/composechallenges/MainActivity.kt
+++ app/app/src/main/java/com/tdiego/composechallenges/MainActivity.kt
@@ -4,26 +4,38 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.tdiego.composechallenges.ui.theme.ComposechallengesTheme
+import androidx.compose.ui.tooling.preview.Preview
+import com.tdiego.composechallenges.shimmering_card.ShimmeringCard
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ComposechallengesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Text(
-                        text = "Empty app template",
-                        modifier = Modifier.padding(innerPadding),
-                    )
-                }
+            App()
+        }
+    }
+}
+
+@Composable
+fun App() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { p ->
+        Column {
+            repeat(4) {
+                ShimmeringCard(Modifier.padding(p))
             }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AppPreview() {
+    App()
 }
